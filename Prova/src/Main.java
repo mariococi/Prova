7ae9d957c12e01fe9c4a68a5cfa9048b110d21e4
Alunno <Alunno@PC-9-INF4>
2017-10-12 10:03:08 +0200

    modifica

--- Prova/src/Main.java
+++ Prova/src/Main.java
@@ -1,10 +1,8 @@
 public class Main{
 	public Main(){
-		System.out.println("porco dio");
 	}
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		Main m=new Main();
 	}
 
 }
